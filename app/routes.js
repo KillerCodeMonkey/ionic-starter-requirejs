744f480b4fb2b2606485c581ff7cdcc8a022e75f
killercodemonkey <bengtler@gmail.com>
2015-09-02 19:13:02 +0200

    initial commit

---
+++ app/routes.js
@@ -0,0 +1,24 @@
+define([
+  'app',
+  // Load Controllers here
+  'controllers/dashboard'
+], function (app) {
+  'use strict';
+  // definition of routes
+  app.config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function ($stateProvider, $urlRouterProvider) {
+      // url routes/states
+      $urlRouterProvider.otherwise('dashboard');
+
+      $stateProvider
+        // app states
+        .state('dashboard', {
+          url: '/dashboard',
+          templateUrl: 'app/templates/dashboard.html',
+          controller: 'DashboardCtrl'
+        });
+    }
+  ]);
+});
